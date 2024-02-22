9ed0ac724ae92f5b474dedd8e792a3ed27d0283e
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-02-22 16:20:35 +0100

    Add index, script, style, README

---
+++ README.md
@@ -0,0 +1,8 @@
+Js-fizzbuzz
+===
+1. Creiamo una costante da lavoro in js per il container di riferimento in html
+2. Iniziamo il ciclo 1-100
+3. Dichiariamo le variabili che si riferiscono alle classi che useremo nel css e html
+4. Iniziamo un unico if che valuti le 3 opzioni di div3, div5 e div3&5, oltre al default
+5. Stampiamo un box con le relative classi per ogni elemento i calcolato
+## 
---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+/*creiamo una costante da lavoro in js per il container di riferimento in html*/
+const containerFzBz = document.getElementById(`containerFizzBuzz`);
+
+/*iniziamo il ciclo 1-100*/
+for (let i = 1; i <= 100; i++) {
+    /*dichiaro le variabili che si riferiscono alle classi che userò nel css e html*/
+    let classDiv3 = '';
+    let classDiv5 = ``;
+    let classDiv3e5 = ``;
+    
+    /*inizio un unico if che valuti le 3 opzioni di div3, div5 e div3&5, oltre al default*/
+    if(i % 3 == 0 || i% 5 == 0){
+    classDiv3 = '';
+    classDiv5 = ``;
+    classDiv3e5 = `div3e5`;
+  } else if ( i % 3 == 0) {
+    classDiv3 = `div3`;
+  } else if ( i % 5 == 0) {
+    classDiv5 = `div5`; 
+  }
+  
+  /*stampo un box con le relative classi per ogni elemento i calcolato*/
+  containerFzBz.innerHTML += 
+  `<div class="fizzBox ${classDiv3} ${classDiv5} ${classDiv3e5}">${i}</div>`
+}
