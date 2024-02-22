c8a315e6ff272f3c3fa7c885cdaaf57411bcad46
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-02-22 16:39:33 +0100

    Fixed erro

--- scripts/script.js
+++ scripts/script.js
@@ -9,17 +9,19 @@ for (let i = 1; i <= 100; i++) {
     let classDiv3e5 = ``;
     
     /*inizio un unico if che valuti le 3 opzioni di div3, div5 e div3&5, oltre al default*/
-    if(i % 3 == 0 || i% 5 == 0){
-    classDiv3 = '';
-    classDiv5 = ``;
+    if(i % 3 == 0 && i% 5 == 0){
     classDiv3e5 = `div3e5`;
+    /*stampo un box con le relative classi per ogni elemento i calcolato*/
+    containerFzBz.innerHTML += 
+    `<div class=" fizzBox ${classDiv3e5}">SASICC E FRIARIELL</div>`
   } else if ( i % 3 == 0) {
     classDiv3 = `div3`;
+    containerFzBz.innerHTML += 
+    `<div class=" fizzBox ${classDiv3}"> SASICC </div>`
   } else if ( i % 5 == 0) {
     classDiv5 = `div5`; 
+    containerFzBz.innerHTML += 
+    `<div class=" fizzBox ${classDiv5}"> FRIARIELL </div>`
   }
-  
-  /*stampo un box con le relative classi per ogni elemento i calcolato*/
-  containerFzBz.innerHTML += 
-  `<div class="fizzBox ${classDiv3} ${classDiv5} ${classDiv3e5}">${i}</div>`
+
 }
